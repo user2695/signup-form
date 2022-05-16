f1f456ec2a60f02de46abf6e018a504fa268d32b
user2695 <zy.khan37@gmail.com>
2022-05-16 19:03:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <form>
+      <div>
+        <h2>Create an account</h2>
+      </div>
+      <br />
+        <div className="label">
+          <label>
+            Your email address:
+            <br />
+            <input className="input" type="email" name="email" />
+          </label>
+        </div>
+        <br />
+        <div className="label">
+          <label>
+            Your password:
+            <br />
+            <input className="input" type="password" name="password" />
+          </label>
+        </div>
+        <br />
+        <div className="label">
+          <label>
+            Confirm your password:
+            <br />
+            <input className="input" type="text" name="name" />
+          </label>
+        </div>
+        <br />
+        <div className="label">
+          <label>
+            Your full name:
+            <br />
+            <input className="input" type="text" name="name" />
+          </label>
+        </div>
+        <br />
+        <div>
+          <label>
+            <input type="checkbox" checked />
+            I read and agree Terms and Conditions
+            <br />
+          </label>
+        </div>
+        <br />
+
+        <button className="btn" type="submit">Create account</button>
+      </form>
     </div>
+
   );
 }
 
---
+++ src/Text.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function Text() {
+  return (
+    <div className="text">
+      <b>Choose a date range</b>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's st
+      </p>
+    </div>
+  );
+}
+
+export default Text;
